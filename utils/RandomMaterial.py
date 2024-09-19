8fde3fd87840ee928957dbcea6220b08df1afa67
rvakato <rvakato>
2024-09-19 14:01:52 +0100

    addon v1

--- utils/RandomMaterial.py
+++
@@ -1,9 +0,0 @@
-import random 
-import bpy 
-prefix = '_' 
-materials = [] 
-for mat in bpy.data.materials: 
-  if mat.name.startswith(prefix): materials.append(mat) 
-
-for obj in bpy.context.selected_objects: 
-  obj.active_material = random.choice(materials)
